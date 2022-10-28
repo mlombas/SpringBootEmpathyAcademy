7ee3db09ac01ce669f85ee8cef0e5c476ddcddf2
Mario Lombas <mariol@empathy.co>
2022-10-28 11:39:14 +0200

    Added title search, half-documented

--- src/main/java/co/empathy/academy/demo_search/services/RestService.java
+++ src/main/java/co/empathy/academy/demo_search/services/RestService.java
@@ -4,9 +4,30 @@ import org.json.JSONObject;
 
 public interface RestService {
 
+    /**
+     * GETs an url, tries to return the specified class
+     *
+     * @param url the url to GET
+     * @param clazz the class to return
+     * @return the return of the REST call
+     * @param <T> the type of the class to return
+     */
     public <T> T getUrl(String url, Class<T> clazz);
 
+    /**
+     * GETs an url, in JSON format
+     *
+     * @param url the url to GET
+     * @return whatever the REST call returns, in json
+     */
     public JSONObject getUrlJSON(String url);
 
+    /**
+     * POSTs a JSON object to an url
+     *
+     * @param url the url to POST to
+     * @param json the body of the call
+     * @return whatever the REST call returns, in json
+     */
     public JSONObject postJSON(String url, JSONObject json);
 }
