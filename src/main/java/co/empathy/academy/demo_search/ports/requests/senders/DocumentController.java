00ac87383632251c38e6c2264f70ceac93a59f5a
Mario Lombas <mariol@empathy.co>
2022-11-10 11:32:09 +0100

    Fixed some errors, now it indexes files

--- src/main/java/co/empathy/academy/demo_search/ports/requests/senders/DocumentController.java
+++ src/main/java/co/empathy/academy/demo_search/ports/requests/senders/DocumentController.java
@@ -6,12 +6,15 @@ import co.empathy.academy.demo_search.ports.requests.PRequestReactor;
 import co.empathy.academy.demo_search.ports.requests.commands.DocumentCommand;
 import co.empathy.academy.demo_search.ports.requests.commands.document.BulkIndexCommand;
 import co.empathy.academy.demo_search.ports.requests.commands.document.MultipleFileBulkIndexCommand;
+import co.empathy.academy.demo_search.ports.requests.commands.document.PipelineIndexCommand;
+import co.empathy.academy.demo_search.ports.requests.commands.document.pipelines.DocumentZipperPipe;
 import co.empathy.academy.demo_search.util.TSVReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.Doc;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -33,16 +36,23 @@ public class DocumentController {
         Path rpath = Paths.get(".", ratings.getOriginalFilename());
         ratings.transferTo(rpath);
 
-        reactor.reactToDocument(
-                new MultipleFileBulkIndexCommand<Movie, Ratings>(
+        var command = new PipelineIndexCommand<Movie>(
+                new TSVReader<>(
+                        new File(bpath.toUri()),
+                        Movie.class,
+                        "genres"
+                )
+        );
+        command.addPipe(
+                new DocumentZipperPipe<Movie, Ratings>(
                         (m, r) -> m
                                 .withAverageRating(r.getAverageRating())
                                 .withNumVotes(r.getNumVotes()),
                         (m, r) -> m.getTconst().equals(r.getTconst()),
-                        new File(bpath.toUri()), new File(rpath.toUri()),
-                        Movie.class, Ratings.class
+                        new TSVReader<>(new File(rpath.toUri()), Ratings.class)
                 )
         );
+        reactor.reactToDocument(command);
 
         return ResponseEntity.ok(null);
     }
