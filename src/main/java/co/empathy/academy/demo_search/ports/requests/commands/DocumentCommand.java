145b8cc9bef643916edac6d3f5ff1debc6f4edc7
Mario Lombas <mariol@empathy.co>
2022-11-14 13:10:08 +0100

    javadoc

--- src/main/java/co/empathy/academy/demo_search/ports/requests/commands/DocumentCommand.java
+++ src/main/java/co/empathy/academy/demo_search/ports/requests/commands/DocumentCommand.java
@@ -1,5 +1,16 @@
 package co.empathy.academy.demo_search.ports.requests.commands;
 
+import java.util.ServiceLoader;
+
+/**
+ * Represents the command to index anything
+ * @param <T> the thing to index
+ */
 public interface DocumentCommand<T> {
+    /**
+     * The documents to index
+     * @return an iterable. Prefereable one which doesn't load in
+     * memory all documents needed, in case those are too many
+     */
     Iterable<T> getDocuments();
 }
--- src/main/java/co/empathy/academy/demo_search/ports/requests/commands/SearchCommand.java
+++ src/main/java/co/empathy/academy/demo_search/ports/requests/commands/SearchCommand.java
@@ -10,11 +10,35 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
-public interface SearchCommand<T> extends Consumer<List<T>> {
+/**
+ * Represents the command to search for anything
+ * @param <T> the thing to search
+ */
+public interface SearchCommand<T> {
+    /**
+     * Builds the query the command wants
+     * @param builder the query builder
+     * @return the query built
+     */
     Query buildQuery(PQueryBuilder builder);
+
+    /**
+     * Builds the filter the command wants
+     * @param builder the filter builder
+     * @return the filter built
+     */
     Query buildFilter(PFilterBuilder builder);
 
+    /**
+     * Future which will be completed when the command finishes
+     * @return the future
+     */
     CompletableFuture<List<T>> getFuture();
-    void accept(List<T> returns);
+
+    /**
+     * Makes the command accept a result
+     * @param what the result of the command
+     */
+    void accept(List<T> what);
     Class<T> getInnerClass();
 }
